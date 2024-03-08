2ca4d265f2c8bf336141533061f9e4d004aa09fc
shankar2409 <138353210+shankar2409@users.noreply.github.com>
2024-03-08 12:00:00 +0530

    Update MakeMytripFlipghtBooking.java

--- src/test/java/advsele/MakeMytripFlipghtBooking.java
+++ src/test/java/advsele/MakeMytripFlipghtBooking.java
@@ -17,28 +17,28 @@ public class MakeMytripFlipghtBooking {
 
 	@Test(groups="smoke")
 	public void accessApp() throws AWTException {
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		driver.get("https://www.makemytrip.com/");
-		driver.findElement(By.cssSelector("[class='headerIcons makeFlex hrtlCenter column active']")).click();
-		driver.findElement(By.cssSelector("[data-cy='oneWayTrip']")).click();
-		WebElement fromcity = driver.findElement(By.id("fromCity"));
-		fromcity.click();
-//		fromcity.clear();
-		fromcity.sendKeys("coimbatore");
-		List<WebElement> fromcitysug = driver.findElements(By.cssSelector("[class='react-autosuggest__suggestions-list']"));
-		for (WebElement webElement : fromcitysug) {
-		if (webElement.getText().equalsIgnoreCase("coimbatore")) {
-			webElement.click();
-		}
-	}
-}
+// 		driver = new ChromeDriver();
+// 		driver.manage().window().maximize();
+// 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+// 		driver.get("https://www.makemytrip.com/");
+// 		driver.findElement(By.cssSelector("[class='headerIcons makeFlex hrtlCenter column active']")).click();
+// 		driver.findElement(By.cssSelector("[data-cy='oneWayTrip']")).click();
+// 		WebElement fromcity = driver.findElement(By.id("fromCity"));
+// 		fromcity.click();
+// //		fromcity.clear();
+// 		fromcity.sendKeys("coimbatore");
+// 		List<WebElement> fromcitysug = driver.findElements(By.cssSelector("[class='react-autosuggest__suggestions-list']"));
+// 		for (WebElement webElement : fromcitysug) {
+// 		if (webElement.getText().equalsIgnoreCase("coimbatore")) {
+// 			webElement.click();
+// 		}
+// 	}
+// }
 			
 		
-//		fromcity.click();
-//		WebElement toCity=driver.findElement(By.id("toCity"));
-//		toCity.clear();
-//		toCity.sendKeys("new Delhi");
-//		toCity.click();
+// //		fromcity.click();
+// //		WebElement toCity=driver.findElement(By.id("toCity"));
+// //		toCity.clear();
+// //		toCity.sendKeys("new Delhi");
+// //		toCity.click();
 	}
